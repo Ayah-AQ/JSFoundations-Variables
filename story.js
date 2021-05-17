528a2f6b68d54d6afc2672aba1e5bffdc863405c
Ayah-AQ <ayoya_1998@hotmail.com>
2021-05-17 16:52:44 +0300

    done

--- story.js
+++ story.js
@@ -1,1 +1,5 @@
-
+let name = "Aya"
+let age = 22
+let hoppy= "drawing"
+ age = (age + 30)
+console.log (`Mr./Mrs. ${name}  in 33 years, you will be ${age} By then you'll have decades of ${hoppy} practice!`)
